68df208c51334e92fe7018e96192d4c24c862dac
thanhdat4804 <thanhdat4804@gmail.com>
2025-09-24 16:19:28 +0700

    de

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { StudentModule } from './modules/student/student.module';
-
-@Module({
-  imports: [PrismaModule, StudentModule],
-})
-export class AppModule {}
--- src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Đặt Global để không phải import nhiều lần ở từng module
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService], // Xuất ra để các module khác có thể inject PrismaService
-})
-export class PrismaModule {}
