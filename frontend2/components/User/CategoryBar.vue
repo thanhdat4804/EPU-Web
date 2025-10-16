1e96fdcc2ed3bf6a895de5c5add4c3189d4ec528
nhakhac17 <nhakhac38@gmail.com>
2025-10-16 17:54:23 +0700

    ddfff

---
+++ frontend2/components/User/CategoryBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="flex justify-center space-x-8 py-3 text-sm font-medium border-b border-gray-200">
+    <a href="#" class="text-blue-600 border-b-2 border-blue-600 pb-1">This week</a>
+    <a href="#" class="hover:text-blue-600">For you</a>
+    <a href="#" class="hover:text-blue-600">Trending</a>
+    <a href="#" class="hover:text-blue-600">Art</a>
+    <a href="#" class="hover:text-blue-600">Interiors</a>
+    <a href="#" class="hover:text-blue-600">Jewellery</a>
+    <a href="#" class="hover:text-blue-600">Watches</a>
+    <a href="#" class="hover:text-blue-600">Fashion</a>
+    <a href="#" class="hover:text-blue-600">Coins & Stamps</a>
+    <a href="#" class="hover:text-blue-600">Comics</a>
+    <a href="#" class="hover:text-blue-600">Cars & Bikes</a>
+  </nav>
+</template>
